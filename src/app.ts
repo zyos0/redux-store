5b6c7ffd062b30b6d94c43c1d139a78742bcf5de
zyos0 <robertomalaver7@gmail.com>
2017-12-21 16:24:51 -0400

    init try

--- src/app.ts
+++ src/app.ts
@@ -1,27 +1,48 @@
-import { renderTodos } from './utils';
+import * as fromStore from './store'
+import {renderTodos} from './utils';
 
 const input = document.querySelector('input') as HTMLInputElement;
 const button = document.querySelector('button') as HTMLButtonElement;
 const destroy = document.querySelector('.unsubscribe') as HTMLButtonElement;
 const todoList = document.querySelector('.todos') as HTMLLIElement;
 
+const reducers = {
+  todos: fromStore.reducer
+};
+
+
+const store = new fromStore.Store(reducers);
+
+console.log(store.value);
+
 button.addEventListener(
   'click',
   () => {
     if (!input.value.trim()) return;
 
-    const payload = { label: input.value, complete: false };
+    const payload = {label: input.value, complete: false};
 
-    console.log(payload);
+    store.dispatch({
+      type: 'ADD_TODO',
+      payload
+    });
 
     input.value = '';
   },
   false
 );
+const unsubscribe = store.subscribe(state => {
+  renderTodos(state.todos.data);
+});
 
-todoList.addEventListener('click', function(event) {
+
+destroy.addEventListener('click',unsubscribe,false);
+
+todoList.addEventListener('click', function (event) {
   const target = event.target as HTMLButtonElement;
   if (target.nodeName.toLowerCase() === 'button') {
     console.log(target);
   }
 });
+
+store.subscribe(state => console.log('STATE:::', state));
